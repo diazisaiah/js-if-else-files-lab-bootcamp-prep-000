7143f18a060306dc0c4889a1614479c994f8338d
Isaiah Diaz <diazisaiah0221@gmail.com>
2018-04-05 23:03:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
-if (name === "Susan") 
+if (name === "Susan")
 {
   console.log("The name is correct")
 } else {
@@ -9,4 +9,3 @@ if (name === "Susan")
 // Don't worry about this
 module.exports = { name, height
 }
-
